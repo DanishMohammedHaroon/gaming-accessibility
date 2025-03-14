1a92eb5563db5b89395079f43bf89ced3b44bfa5
Danish Haroon <danishmohammedharoon@gmail.com>
2025-03-13 23:49:59 -0230

    addresses the svg eact coponent issues

--- src/pages/NeurodivergencePage/NeurodivergencePage.jsx
+++ src/pages/NeurodivergencePage/NeurodivergencePage.jsx
@@ -1,17 +1,14 @@
 import "./NeurodivergencePage.scss";
+import WindowsLogo from "../../components/WindowsLogo/WindowsLogo.jsx";
+import XboxLogo from "../../components/XboxLogo/XboxLogo.jsx";
 import React from "react";
-
 
 function NeuroDivergencePage() {
   return (
     <div className="neuro__page">
       <div className="neuro__logos">
-        <img
-          src="public/images-logos/xbox.svg"
-          alt="x-box logo"
-          className="neuro__xbox"
-        />
-        <img src="public/images-logos/windows.svg" alt="windows logo" className="neuro__windows" />
+        <WindowsLogo className="neuro__xbox" />
+        <XboxLogo className="neuro__windows" />
       </div>
 
       <div className="image-section">
