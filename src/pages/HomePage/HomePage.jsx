611796426c0af814b9f69129ae08693716b1053b
christinz <christinyzhong@gmail.com>
2025-03-14 11:32:52 -0400

    add hover effect to games

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,67 @@
 import CognitivePage from "../CognitivePage/CognitivePage";
 import "./HomePage.scss";
-import { Link } from "react-router-dom";
-import { useState } from "react";
 import Header from "../../components/Header/Header";
 
 export default function HomePage({}) {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const toggleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
-
   return (
     <>
       <div className="homepage">
         <Header />
         <section className="games">
-          <img
-            className="games__starfield"
-            src="src\assets\images\starfield.png"
-          ></img>
-          <img
-            className="games__img"
-            src="src\assets\images\fortnite.png"
-          ></img>
-          <img className="games__img" src="src\assets\images\diablo.png"></img>
-          <img className="games__img" src="src\assets\images\mirage.png"></img>
-          <img
-            className="games__img"
-            src="src\assets\images\ghostwire.png"
-          ></img>
-          <img className="games__img" src="src\assets\images\forza.png"></img>
-          <img
-            className="games__img"
-            src="src\assets\images\minecraft.png"
-          ></img>
-          <img className="games__img" src="src\assets\images\disney.png"></img>
-          <img
-            className="games__img"
-            src="src\assets\images\partyanimals.png"
-          ></img>
+          <div>
+            <img
+              className="games__starfield"
+              src="src\assets\images\starfield.png"
+            ></img>
+          </div>
+
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\fortnite.png"
+            ></img>
+          </div>
+
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\diablo.png"
+            ></img>
+          </div>
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\mirage.png"
+            ></img>
+          </div>
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\ghostwire.png"
+            ></img>
+          </div>
+          <div className="games__container">
+            <img className="games__img" src="src\assets\images\forza.png"></img>
+          </div>
+
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\minecraft.png"
+            ></img>
+          </div>
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\disney.png"
+            ></img>
+          </div>
+          <div className="games__container">
+            <img
+              className="games__img"
+              src="src\assets\images\partyanimals.png"
+            ></img>
+          </div>
         </section>
         <section className="options">
           <img
