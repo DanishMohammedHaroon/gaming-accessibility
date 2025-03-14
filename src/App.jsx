b95c3be9f33ee14dbeb9ff09ae7bf1f33cd6c52b
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-14 00:25:47 -0400

    merge changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.scss";
 import "./styles/partials/_global.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/HomePage/HomePage";
-import MotorPage from "./pages/MotorPage/MotorPage";
-import NeuroDivergencePage from "./pages/NeurodivergencePage/NeurodivergencePage";
+// import HomePage from "./pages/HomePage/HomePage";
+// import MotorPage from "./pages/MotorPage/MotorPage";
+// import NeuroDivergencePage from "./pages/NeurodivergencePage/NeurodivergencePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CognitivePage from "./pages/CognitivePage/CognitivePage";
+// import CognitivePage from "./pages/CognitivePage/CognitivePage";
 import HearingPage from "./pages/HearingPage/HearingPage";
 
 function App() {
@@ -15,11 +15,11 @@ function App() {
       <Header />
       <main className="main">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} /> */}
           {/* temporary route */}
-          <Route path="/motor" element={<MotorPage />} />
+          {/* <Route path="/motor" element={<MotorPage />} />
           <Route path="/neuro" element={<NeuroDivergencePage />} />
-          <Route path="/cognitive" element={<CognitivePage />} />
+          <Route path="/cognitive" element={<CognitivePage />} /> */}
           <Route path="/" element={<HearingPage />} />
         </Routes>
       </main>
