3b5cc085aa788cd85727511b6deb4942f908bcf0
reneecwl <reneewlcheng@gmail.com>
2025-03-13 20:11:38 -0400

    Add Cognitive page

--- src/pages/CognitivePage/CognitivePage.jsx
+++ src/pages/CognitivePage/CognitivePage.jsx
@@ -1,1 +1,31 @@
+import React, { useState } from "react";
 import "./CognitivePage.scss";
+import cognitive1 from "../../../public/Starfield_IntoTheStarfield_Wallpaper_2560x1440_01.webp";
+
+export default function CognitivePage() {
+  const [showAfterAI, setShowAfterAI] = useState(false);
+
+  const toggleText = () => {
+    setShowAfterAI(!showAfterAI);
+  };
+
+  return (
+    <>
+      <h2>This is the Cognitive Page</h2>
+      <div className="cognitive__before-image">
+        <img src={cognitive1} alt="Starfield Into The Starfield Wallpaper" />
+
+        <div className="overlay">
+          <div className="overlay-content">
+            <p className={showAfterAI ? "cognitve__narrative--after" : "cognitive__narrative"}>
+              {showAfterAI
+                ? "Time is breaking. Creatures are attacking. Get out now."
+                : "Time anomalies are wreaking havoc in Sector Gamma-9 due to 'The Nexus' artifact. Researchers are experiencing strange aging and memory issues, while local creatures have turned aggressive. Emergency evacuation is underway."}
+            </p>
+            <button onClick={toggleText}>{showAfterAI ? "Show Before AI" : "Show After AI"}</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import CognitivePage from "../CognitivePage/CognitivePage";
 import "./HomePage.scss";
 
 export default function HomePage({}) {
@@ -5,6 +6,7 @@ export default function HomePage({}) {
     <>
       <div>
         <h3 className="homepage__title">Team Misfit- HomePage</h3>
+        <CognitivePage />
       </div>
     </>
   );
